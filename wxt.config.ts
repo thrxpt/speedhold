e8f532f052a7420d6a58194e7d7ea13454df7b14
Theeraphat Jaingam <jain.theera@gmail.com>
2026-04-27 05:59:39 +0700

    feat: add icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-vue"],
+  modules: ["@wxt-dev/module-vue", "@wxt-dev/auto-icons"],
   manifest: {
     name: "Hold to Speed Up",
     permissions: ["storage"],
